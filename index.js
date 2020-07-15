6cf5f0f27ceb242002403d993c1760c5ab44680b
fishy15 <aaryan.prakash3.14@gmail.com>
2020-07-14 22:07:28 -0700

    add :catthink: posting

--- index.js
+++ index.js
@@ -8,10 +8,24 @@ client.once('ready', () => {
 
 client.on('message', message => {
     if (message.author.bot) return;
-    if (message.content === 'ping') {
+    const content = message.content;
+    if (content === 'ping') {
         message.channel.send('pong');
-    } else if (message.content == 'pong') {
+    } else if (content === 'pong') {
         message.channel.send('ping');
+    }
+
+    if (content.startsWith('!lgm')) {
+        if (content.length <= 5) {
+            message.channel.send('smh');
+        } else {
+            const person = content.substring(content.search(' ') + 1).trim();
+            message.channel.send(`yes ${person} orz`);
+        }
+    }
+
+    if (content.toLowerCase().search('catthink') !== -1) {
+        message.channel.send('<:catthink:701386574251950141>');
     }
 });
 
